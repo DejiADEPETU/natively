e1d32abc283bd547a28c45ed85b3da64005d587a
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-06-02 02:32:21 -0400

    -

--- server/server.js
+++ server/server.js
@@ -1,33 +1,38 @@
 import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import config from './config';
-import userRoute from './routes/userRoute';
-import productRoute from './routes/productRoute';
-import orderRoute from './routes/orderRoute';
+import data from './data';
+// import path from 'path';
+// import mongoose from 'mongoose';
+// import bodyParser from 'body-parser';
+// import config from './config';
+// import userRoute from './routes/userRoute';
+// import productRoute from './routes/productRoute';
+// import orderRoute from './routes/orderRoute';
 
-const mongodbUrl = config.MONGODB_URL;
-mongoose.connect(mongodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).catch((error) => console.log(error.reason));
+// const mongodbUrl = config.MONGODB_URL;
+// mongoose.connect(mongodbUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// }).catch((error) => console.log(error.reason));
 
 
-const app = express();
-app.use(bodyParser.json());
+// const app = express();
+// app.use(bodyParser.json());
 
-app.use('/api/users', userRoute);
-app.use('/api/products', productRoute);
-app.use('/api/orders', orderRoute);
-app.get('/api/config/paypal', (req, res) => {
-  res.send(config.PAYPAL_CLIENT_ID);
-});
+// app.use('/api/users', userRoute);
+// app.use('/api/products', productRoute);
+// app.use('/api/orders', orderRoute);
+// app.get('/api/config/paypal', (req, res) => {
+//   res.send(config.PAYPAL_CLIENT_ID);
+// });
 
-app.use(express.static(path.join(__dirname, '/../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+// app.use(express.static(path.join(__dirname, '/../client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+// });
+
+app.get('/api/products", (req, res) => {
+    res.send(data.products);
 });
 
 app.listen(config.PORT, () => { console.log('Server started at http://localhost:5000'); });
